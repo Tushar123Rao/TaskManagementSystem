17d4d3cd91ac5b74993f6a138c6b3306b2ea4748
Tushar Rao <tusharrao21@gmail.com>
2026-03-31 17:00:08 +0530

    Build task CRUD foundation with create and complete actions

---
+++ TaskManagementSystem/Controllers/TaskController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TaskManagementSystem.Data;
+using TaskManagementSystem.Models;
+using TaskManagementSystem.ViewModels;
+using TaskStatus = TaskManagementSystem.Models.Enums.TaskStatus;
+
+namespace TaskManagementSystem.Controllers
+{
+    public class TaskController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public TaskController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var tasks = _context.TaskItems.ToList();
+            return View(tasks);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(CreateTaskViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var taskitem = new TaskItem
+                {
+                    Title = model.Title,
+                    Description = model.Description,
+                    Status = model.Status,
+                    Priority = model.Priority,
+                    DueDate = model.DueDate,
+                    ReminderDateTime = model.ReminderDateTime,
+                    IsCompleted = false,
+                    CreatedAt = DateTime.Now,
+                    UpdatedAt = null,
+                    UserId = "temp-user"
+                };
+                _context.TaskItems.Add(taskitem);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+
+        public IActionResult MarkComplete(int id)
+        {
+            var item = _context.TaskItems.FirstOrDefault(i => i.Id == id);
+            if(item == null)
+            {
+                return NotFound();
+            }
+            item.IsCompleted = true;
+            item.Status = TaskStatus.Completed;
+            item.UpdatedAt = DateTime.Now;
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ TaskManagementSystem/ViewModels/CreateTaskViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using TaskStatus = TaskManagementSystem.Models.Enums.TaskStatus;
+using TaskPriority = TaskManagementSystem.Models.Enums.TaskPriority;
+namespace TaskManagementSystem.ViewModels
+{
+    public class CreateTaskViewModel
+    {
+        [Required]
+        [StringLength(100, MinimumLength = 4)]
+        public string Title { get; set; } = null!;
+
+        [MaxLength(100)]
+        public string? Description { get; set; }
+
+        public TaskPriority Priority { get; set; }
+        public TaskStatus Status { get; set; }
+
+        public DateOnly? DueDate { get; set; }
+        public DateTime? ReminderDateTime { get; set; }
+    }
+}
